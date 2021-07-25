6dad82e72ba5f8af19be20a26db2d16d1c0cf168
Shacdemort <mohamed-wadhah.dahouathi@ensta-paris.fr>
2021-07-25 14:22:24 +0200

    finished the draw_action function which makes player draw his own move

--- main.py
+++ main.py
@@ -2,67 +2,76 @@ import pyglet
 from pyglet.graphics import vertex_list
 from pyglet.libs.x11.xlib import None_
 from pyglet.text import Label
-
-number_of_X = 3
+import numpy as np
+from math import pi
+size = 3
 width=480
 height=480
-W=height/number_of_X
+W=height/size
 
 class Player():
-    def __init__(self,state) :
-        self.state = state #can be an X or E
-        self.i =None
-        self.j =None
-        self.clicking=False
+    """ class player contain the type of the player"""
+    def __init__(self,state,type) :
+        self.state = state #can be an X or O
+        self.type  = type #humain_player, AI_player, random_player
+       
     
-    def draw_action(self):
-        i=self.i
-        j=self.j
-
-        if  self.clicking:
-            if self.state == 'x':
-                batch=pyglet.graphics.Batch()
-                line1=pyglet.shapes.Line(W*i,W*j,W*(i+1),W*(j+1),color=(255,0,0),batch=batch)
-                line2=pyglet.shapes.Line(W*i,W*(j+1),W*(i+1),W*j,color=(255,0,0),batch=batch)
-                batch.draw()
-                
-            else: pass
-
+    def draw_action(self,i,j):
+        i,j=j,i # permutation cos the pyglet orientation is reversed 
+        batch=pyglet.graphics.Batch()
+        if self.state == 'X':
+            
+            line1=pyglet.shapes.Line(W*(i),W*(size-j),W*(i+1),W*(size-j-1),color=(255,0,0),batch=batch)
+            #line1=pyglet.shapes.Line(W*(size-j),W*(i),W*(size-j-1),W*(i+1),color=(255,0,0),batch=batch)
+            line2=pyglet.shapes.Line(W*(i),W*(size-j-1),W*(i+1),W*(size-j),color=(255,0,0),batch=batch)
+            #line2=pyglet.shapes.Line(W*(size-j-1),W*(i),W*(size-j),W*(i+1),color=(255,0,0),batch=batch)
+            
+            
+        else:
+            #cercle=pyglet.shapes.Circle(W*(i+1/2),W*(size-j-1/2),W/2,color=(255,0,60),batch=batch)
+            cercle=pyglet.shapes.Arc(W*(i+1/2),W*(size-j-1/2),W/2.1,angle=2*pi,start_angle=0,closed=True,color=(0,0,255),batch=batch)
+        batch.draw()
+    
+    def check_win(self,matrix):
+        pass
 
 class Window (pyglet.window.Window):
-    def __init__(self):
-        super(Window, self).__init__()
+    def __init__(self, X_player, O_player):
+        super(Window, self).__init__(width, height, 'Tic Tac Toe')
         self.batch =  pyglet.graphics.Batch()
-        print(self.height)
-        title='Tic Tac Toe'
-        self.set_size(width, height)
-        self.set_caption(title)
-        self.current_player=Player('x')
-    def draw_lines(self):
+        self.matrix= np.zeros((size,size)) # represent the Grid initialized to 0 
+                                            # X==>1 O ==>2
+        self.matrix[2,0]=1
+        print(self.matrix)
+        self.current_player=O_player #first player is the X player
+
+    
+    def draw_Grid(self):
         batch=self.batch
-        W=height/number_of_X
-        for i in range(number_of_X+1):
+        W=height/size
+        for i in range(size+1):
             line1=pyglet.shapes.Line(W*i,0,W*i,height, 5, color=(50,40,30), batch=batch)
             line2=pyglet.shapes.Line(0,W*i,width,W*i, 5, color=(50,40,30), batch=batch)
             self.batch.draw()
 
+   
     def on_mouse_motion(self, x, y, dx, dy):
         pass
     def on_mouse_press(self, x, y, button, modifiers):
         print(x/width,y/height)
-        self.current_player.clicking = True
-        self.current_player.i=0
-        self.current_player.j=0
+        
 
     
     def on_draw(self):
-        
         self.clear()
-        self.draw_lines()
-        self.current_player.draw_action()
+        self.draw_Grid()
+        self.current_player.draw_action(0,2)
+
         
 if __name__ == '__main__':
-    window = Window()
+    X_player=Player('X','random')
+    O_player=Player('O','random')
+    window = Window(X_player, O_player)
     pyglet.app.run()
 
 
